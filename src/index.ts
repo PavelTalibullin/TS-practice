e2cbde52236d6e416f408361136b79d6e3ea9c9d
PavelTalibullin <pavlotalibullin@gmail.com>
2024-12-24 16:41:46 +0300

    lessons 1-5

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { sum, sub, mul, div } from "./math/index";
+
+console.log(sum(1, 2));
+console.log(sub(1, 2));
+console.log(mul(1, 2));
+console.log(div(1, 2));
+
+console.log('Hello Typescriptываы');
